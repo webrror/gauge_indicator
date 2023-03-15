5a89d8e5782eb5a119a984718e65e0f6116c0c49
Jerin <jerinjacob0206@gmail.com>
2023-03-15 12:11:57 +0530

    test1

--- lib/src/utils/calculate_rounded_path.dart
+++ lib/src/utils/calculate_rounded_path.dart
@@ -58,7 +58,7 @@ Path calculateRoundedArcPath(
     )
     ..arcToPoint(
       endInnerPoint,
-      radius: const Radius.circular(0),
+      radius: const Radius.circular(thickness),
     )
     ..arcToPoint(
       startInnerPoint,
@@ -68,7 +68,7 @@ Path calculateRoundedArcPath(
     )
     ..arcToPoint(
       startOuterPoint,
-      radius: const Radius.circular(0),
+      radius: const Radius.circular(thickness),
     );
 
   return axisSurface;
